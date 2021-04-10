a5b176d42fcd1df170c1f0a057ec60d1fb43497c
fuku-youtube <sakurai57.shun@gmail.com>
2021-04-10 22:51:54 +0900

    create account lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,9 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-// 1. Authの確認・実装
-// 2. ログイン後ページの作成
-// 3. ミドルウェアの設定
+use App\Models\User;
 
 class AuthController extends Controller
 {
@@ -28,12 +26,42 @@ class AuthController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
+        // ①アカウントがロックされていたら弾く
+        $user = User::where('email', '=', $credentials['email'])->first();
 
-            return redirect()->route('home')->with('success', 'ログイン成功しました！');
+        if (!is_null($user)) {
+            if ($user->locked_flg === 1) {
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされています。',
+                ]);
+            }
+
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                // ②成功したらエラーカウントを0にする
+                if ($user->error_count > 0) {
+                    $user->error_count = 0;
+                    $user->save();
+                }
+
+                return redirect()->route('home')->with('success', 'ログイン成功しました！');
+            }
+    
+            // ③ログイン失敗したらエラーカウントを1増やす
+            $user->error_count = $user->error_count + 1;
+            // ④エラーカウントが6以上の場合はアカウントをロックする
+            if ($user->error_count > 5) {
+                $user->locked_flg = 1;
+                $user->save();
+
+                return back()->withErrors([
+                    'danger' => 'アカウントがロックされました。解除したい場合は運営者に連絡してください。',
+                ]);
+            }
+            $user->save();
+        
         }
-
+        
         return back()->withErrors([
             'danger' => 'メールアドレスかパスワードが間違っています。',
         ]);
